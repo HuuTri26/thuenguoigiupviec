2611e7d6b6a1b190712944f97c65c13fc33131c0
dinhquoctoan <dinhquoctoan200103@gmail.com>
2024-04-08 14:38:18 +0700

    Thêm AccountService

---
+++ src/main/java/ptithcm/service/AccountService.java
@@ -0,0 +1,16 @@
+package ptithcm.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import ptithcm.entity.AccountEntity;
+
+@Service
+@Transactional
+public interface AccountService {
+	public void addAccount(AccountEntity acc);
+	public void updateAccount(AccountEntity acc);
+	public boolean isExistAccount(String email, String password);
+	public boolean getStatusFromAccount(String email, String password);
+}
